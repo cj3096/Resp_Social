cb58b257290a4bac5d6f87ddb9900a44f7fab977
cj3096 <cesar_quesada@usmp.pe>
2019-08-13 10:08:32 -0500

    1er commit

---
+++ inc/header.php
@@ -0,0 +1,87 @@
+
+
+<!-- 
+<ul class="nav justify-content-end">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">INICIO</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">NOSOTROS</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">PROYECTOS</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">SOCIOS ESTRATÉGICOS</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">VOLUNTARIADO</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">CONTÁCTENOS</a>
+  </li>
+</ul>
+ -->
+
+
+<!-- <nav class="menu">
+    <div class="menu-container">
+        <ul class="menu-list">
+            <li class="menu-item">
+                INICIO
+            </li>
+
+            <li class="menu-item">
+                NOSOTROS    
+            </li>
+
+            <li class="menu-item">
+                PROYECTOS
+            </li>
+
+            <li class="menu-item">
+                SOCIOS ESTRATÉGICOS
+            </li>
+
+            <li class="menu-item">
+                VOLUNTARIADO
+            </li>
+
+            <li class="menu-item">
+                CONTÁCTENOS
+            </li>
+
+        </ul>
+    </div>
+    <a class="menu-toogle">
+       <span class="menu-hamburguer">
+
+       </span>
+    </a>
+</nav> -->
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Navbar</a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Responsabilidad Social</title>
+
+  
+    <link rel="icon" href="img/logousmp.ico">
+    <!--styles -->
+    <link href="css/all.css" rel="stylesheet"> 
+    <link href="css/styles.css" rel="stylesheet"> 
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+
+
+
+<body>
+
+
+<?php include ('inc/header.php') ?>
+
+
+<script src="js/jquery-3.3.1.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
