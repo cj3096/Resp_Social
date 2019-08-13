cd992a4b9f1f1c73e13f8caca5b29d2a5bf5b7af
cj3096 <cesar_quesada@usmp.pe>
2019-08-13 13:05:41 -0500

    fix-slider

--- inc/slider.php
+++ inc/slider.php
@@ -1,15 +1,62 @@
 <div id="carrusel-usmp" class="carousel slide d-none d-md-block" data-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active" data-interval="6000">
-      <img src="./img/slider/1.jpg" class="d-block w-100" alt="...">
+
+  <!-- <div class="row" style="display:flex"> -->
+    <div class="carousel-item active slider1" data-interval="6000" data-pause="hover">
+      <div class="texto-fade centrar">
+
+          <div class="row">
+              <div class="col-md-12">
+                    <h1>Sé parte de nuestro equipo <br> de voluntariado USMP</h1>
+             </div>
+              <div class="col-md-12">
+                    <div class="boton-slide centrar">
+                    <a href="#">
+                    Inscripciones
+                    </a>
+                    </div>
+              </div>
+          </div>
+
+      </div>
     </div>
-    <div class="carousel-item" data-interval="6000">
-      <img src="./img/slider/1.jpg" class="d-block w-100" alt="...">
+
+    <div class="carousel-item slider2" data-interval="6000" data-pause="hover">
+      <div class="texto-fade centrar">
+           <div class="row">
+              <div class="col-md-12">
+                    <h1>Sé parte de nuestro equipo <br> de voluntariado USMP</h1>
+             </div>
+              <div class="col-md-12">
+                    <div class="boton-slide centrar">
+                    <a href="#">
+                    Inscripciones
+                    </a>
+                    </div>
+              </div>
+          </div>
+      </div>
     </div>
-    <div class="carousel-item" data-interval="6000">
-      <img src="./img/slider/1.jpg" class="d-block w-100" alt="...">
+
+    <div class="carousel-item slider3" data-interval="6000" data-pause="hover">
+      <div class="texto-fade centrar">
+      <div class="row">
+              <div class="col-md-12">
+                    <h1>Sé parte de nuestro equipo <br> de voluntariado USMP</h1>
+             </div>
+              <div class="col-md-12">
+                    <div class="boton-slide centrar">
+                    <a href="#">
+                    Inscripciones
+                    </a>
+                    </div>
+              </div>
+          </div>
+      </div>
     </div>
   </div>
+
+  <!-- </div> -->
   <a class="carousel-control-prev" href="#carrusel-usmp" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
